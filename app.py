3a7b4f30d16b5364f0487880190b3d22420f8e31
ajith-01-97 <ajithsilva513@gmail.com>
2024-12-11 12:41:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
-
+ 
 @app.after_request
 def after_request(response):
     response.headers["Access-Control-Allow-Origin"] = "*"
@@ -9,6 +9,6 @@ def after_request(response):
     return response
 @app.route('/')
 def hello_world():
-    return 'Hello from Ajith ECS Container'
+    return 'Hello from karke ECS Container'
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=80)
