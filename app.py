ef167e7d6559125bdaafe12a502565bae0a3840a
ajith-01-97 <ajithsilva513@gmail.com>
2024-12-11 12:41:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def after_request(response):
     return response
 @app.route('/')
 def hello_world():
-    return 'Hello from karke ECS Container'
+    return 'Hello from silva ECS Container'
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
